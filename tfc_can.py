c5f53ecd6f42cbc5b166722efc48f40602ddc8d0
tangyapeng <986827589@qq.com>
2024-07-19 19:16:56 +0800

    feat: Test tfc_can.py passed. 1. Add getTestTesult function, and work fine. 2. Without canfd.

--- tfc_can.py
+++ tfc_can.py
@@ -1,5 +1,5 @@
 ###
-import time, os, sys
+import time, os, sys,string
 import random
 import threading
 
@@ -11,12 +11,14 @@ class TfcZlgCan(): #Basic class DevIf
     __gZCan = None
     __gDevHandle = INVALID_DEVICE_HANDLE
     __defaultConfig = {"can_dev_type":ZCAN_USBCAN2,"can_channel":0, "can_baudrate":500000,
-                       "can_type":ZCAN_TYPE_CAN}
+                       "can_type":ZCAN_TYPE_CAN, "test_frame_id":0x100, "receive_timeout_s":5}
 
     def __init__(self, config = __defaultConfig ):
         print(config, type(config))
         self.tfcCanChanHandle = None
         self.configInfo = config
+        self.rcvTimeout = config['receive_timeout_s']
+
         if TfcZlgCan.__gZCan == None:
             TfcZlgCan.__gZCan = ZCAN()
 
@@ -78,31 +80,97 @@ class TfcZlgCan(): #Basic class DevIf
         if rcv_num > 0:
             print("Receive CAN message number: %d" %rcv_num)
             rcv_msg, rcv_num = TfcZlgCan.__gZCan.Receive(self.tfcCanChanHandle, rcv_num = rcv_num)
-            print(" Received data number is :", rcv_num, "Type is :", type(rcv_msg))
-            dictRslt = {"num":rcv_num, "msg":rcv_msg}
-            
+            listFrames = []
+            for i in range(rcv_num):
+                data = []
+                for j in range(rcv_msg[i].frame.can_dlc):
+                    data.append(rcv_msg[i].frame.data[j])
+                listFrames.append({'id':rcv_msg[i].frame.can_id, 'len':rcv_msg[i].frame.can_dlc, 'data':data})
+            '''
             for i in range(rcv_num):
                 print("[%d]:timestamps:%d,type:CAN, id:%s, dlc:%d, eff:%d, rtr:%d, data:%s" %(i, rcv_msg[i].timestamp, 
                       hex(rcv_msg[i].frame.can_id), rcv_msg[i].frame.can_dlc, 
                       rcv_msg[i].frame.eff, rcv_msg[i].frame.rtr,
                       ''.join(hex(rcv_msg[i].frame.data[j])+ ' 'for j in range(rcv_msg[i].frame.can_dlc))))
-            return dictRslt
+                        
+            '''
 
-    def write(self):
-        #Send CAN Messages
-        transmit_num = 10
-        msgs = (ZCAN_Transmit_Data * transmit_num)()
-        for i in range(transmit_num):
-            msgs[i].transmit_type = 2 #0-正常发送，2-自发自收
-            msgs[i].frame.eff     = 0 #0-标准帧，1-扩展帧
-            msgs[i].frame.rtr     = 0 #0-数据帧，1-远程帧
-            msgs[i].frame.can_id  = i
-            msgs[i].frame.can_dlc = 8
-            for j in range(msgs[i].frame.can_dlc):
-                msgs[i].frame.data[j] = j
-        ret = TfcZlgCan.__gZCan.Transmit(self.tfcCanChanHandle, msgs, transmit_num)
+            return listFrames
+
+    def write(self, data = None):
+        if not isinstance(data, str):
+            print("The type of data is error.")
+            exit(0)
+
+        if len(data) == 0:
+            print("The type of data error.")
+            exit(0)
+        data += '.'
+        byteData = data.encode()
+        dataRcv = self.__data2Frames(byteData)
+        ret = TfcZlgCan.__gZCan.Transmit(self.tfcCanChanHandle, dataRcv[1], dataRcv[0])
         print("Tranmit Num: %d." % ret)
 
+    def __data2Frames(self, data):
+        if not isinstance(data, (bytes, bytearray)):
+            return None
+        
+        if len(data) % 8:
+            transmit_num = len(data) // 8 + 1
+            msgs = (ZCAN_Transmit_Data * transmit_num)()
+            for i in range(transmit_num):
+                msgs[i].transmit_type = 2 #0-正常发送，2-自发自收
+                msgs[i].frame.eff     = 0 #0-标准帧，1-扩展帧
+                msgs[i].frame.rtr     = 0 #0-数据帧，1-远程帧
+                msgs[i].frame.can_id  = i
+                if i == (transmit_num - 1):
+                    msgs[i].frame.can_dlc = len(data) % 8
+                else:
+                    msgs[i].frame.can_dlc = 8
+                
+                for j in range(msgs[i].frame.can_dlc):
+                    msgs[i].frame.data[j] = data[8 * i + j]
+        else:
+            transmit_num = len(data) // 8 
+            msgs = (ZCAN_Transmit_Data * transmit_num)()
+            for i in range(transmit_num):
+                msgs[i].transmit_type = 2 #0-正常发送，2-自发自收
+                msgs[i].frame.eff     = 0 #0-标准帧，1-扩展帧
+                msgs[i].frame.rtr     = 0 #0-数据帧，1-远程帧
+                msgs[i].frame.can_id  = i
+                msgs[i].frame.can_dlc = 8
+                
+                for j in range(msgs[i].frame.can_dlc):
+                    msgs[i].frame.data[j] = data[8 * i + j]
+
+        return [transmit_num, msgs]
+
+    def getTestResult(self):
+
+        '''
+        TODO: Add a configure segment to identify the frame ID
+
+        '''
+        strRcv = ''
+        startTime = time.time()
+        endTime = self.rcvTimeout + startTime
+        print("timeout is :", self.rcvTimeout)
+
+        while True:
+            if time.time() <= endTime:
+                tmpFrames = self.read()
+                if len(tmpFrames) > 0:
+                    for frame in tmpFrames:
+                        # if frame['id'] == self.configInfo["test_frame_id"]:
+                        if True: #bytes(frame['data']).decode()
+                            strRcv += bytes(frame['data']).decode()
+                if strRcv.endswith('.'):
+                    print( strRcv)
+                    break
+            else:
+                break
+        return strRcv
+    
     def close(self):
         TfcZlgCan.__gZCan.ResetCAN(self.tfcCanChanHandle)
         print("Finish")
@@ -122,18 +190,26 @@ def testCanIf():
     thread.start()
 
     myCanConfig = {"can_dev_type":ZCAN_USBCAN2,"can_channel":0, "can_baudrate":500000,
-                       "can_type":ZCAN_TYPE_CANFD}
+                       "can_type":ZCAN_TYPE_CANFD, "test_frame_id":0x100}
     myCan = TfcZlgCan()
     myCan.open()
-    myCan.write()
-    while True:
-        rslt = myCan.read()
-        if rslt['num']:
-            print("Result is : ", rslt)
-            break
+
+    dataSend = ""
+    for i in range(20):
+        dataSend += 'Today is a nice day.\n'
+    myCan.write(data=dataSend)
+    myCan.getTestResult()
+    # while True:
+    #     frames = myCan.read()
+    #     if len(frames):
+    #         print("\nResult is : ")
+    #         for frame in frames:
+    #             # print("ID:{} Length:{} data{}".format(frame['id'], frame['len'], bytes(frame['data']).decode()))
+    #             print(bytes(frame['data']).decode())
+    #         break
         
-        if thread.is_alive() == False:
-            break
+    #     if thread.is_alive() == False:
+    #         break
     
     myCan.closeDev()
 
